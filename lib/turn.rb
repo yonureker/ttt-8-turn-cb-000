31f98cc9dcf4daa8bb3910a4bc3a83bc0ae2866e
Onur Eker <yonureker@gmail.com>
2017-12-08 18:26:53 +0000

    automatically backed up by learn

--- lib/turn.rb
+++ lib/turn.rb
@@ -1,9 +1,9 @@
 def turn(board)
   puts "Please enter 1-9:"
-  user_input = gets.chomp
+  user_input = gets.strip
   index = input_to_index(user_input)
-  if (valid_move?(board, index) == true)
-    move (board, index, char = "X")
+  if valid_move?(board, index)
+    move (board, index)
     display_board(board)
   else turn(board)
   end
@@ -18,7 +18,7 @@ def display_board(board)
 end
 
 def valid_move?(board, index)
-  if (index.between?(0,8) == true)
+  if index.between?(0,8)
     if (board[index] == " " || board[index] == "" || board[index] == nil)
       return true
     else
